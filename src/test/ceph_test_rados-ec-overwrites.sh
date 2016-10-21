e938b0e682a26f274b8fbcda49d390441ef826e8
Samuel Just <sjust@redhat.com>
2016-10-21 14:29:43 -0700

    DNM: make it 10k operations

--- src/test/ceph_test_rados-ec-overwrites.sh
+++ src/test/ceph_test_rados-ec-overwrites.sh
@@ -19,4 +19,4 @@ source $(dirname $0)/detect-build-env-vars.sh
 
 CEPH_CLI_TEST_DUP_COMMAND=1 \
 EC_OVERWRITES=1 MON=1 OSD=5 CEPH_START='mon osd' CEPH_PORT=7401 $CEPH_ROOT/src/test/vstart_wrapper.sh \
-    $CEPH_BIN/ceph_test_rados --pool ec --no-omap --no-sparse --max-in-flight 7 --op write 50 --op write_excl 0 --op append_excl 0 --op append 50 --objects 15 --op read 100 --op snap_create 10 --op snap_remove 10 --op rollback 10 --op delete 50 --max-ops 1000
+    $CEPH_BIN/ceph_test_rados --pool ec --no-omap --no-sparse --max-in-flight 7 --op write 50 --op write_excl 0 --op append_excl 0 --op append 50 --objects 15 --op read 100 --op snap_create 10 --op snap_remove 10 --op rollback 10 --op delete 50 --max-ops 10000
